e78ab93cb5177d8843227ff46c9401a707e9e91b
Just In <70413779+jkhclimited@users.noreply.github.com>
2025-07-23 21:23:00 -0400

    Add files via upload

---
+++ 2-Dice-RNG.py
@@ -0,0 +1,22 @@
+# User inputs number of sides on the die
+# User decides to roll or not
+# Active code goes inside an infinite loop
+
+import random
+
+# User input for number of faces on the die
+sides = input('How many sides on the die? (Integers only): ')
+if sides.isnumeric() == True:
+    print('Press enter to roll. Input X to exit.')
+    while True:
+        choice = input('Press enter to roll. ')
+        if choice == '':
+            result = random.randint(1, int(sides))
+            print(f'({result})')
+        elif choice == 'x' or choice == 'X':
+            print('Thank you for playing.')
+            break
+        else:
+            print('Invalid input inner.')
+else:
+    print('Invalid input.')
